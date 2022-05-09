0fe986cc4de153de39739361938ee61950c34d05
schadrack <schadrackbotombe@gmail.com>
2022-05-09 13:58:31 +0200

    created basic template

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
          <h1>Home Page</h1>
+         <p>Insert code here--------</p>
     </div>
   )
 }
